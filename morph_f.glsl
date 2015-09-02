e12ce50ea65ef453fff73c4f2e5cb15e6fbba124
wezu <grzechotnik1984@gmail.com>
2015-09-02 18:35:43 +0200

    gl_LightSourceParameters ->p3d_LightSourceParameters

--- morph_f.glsl
+++ morph_f.glsl
@@ -1,10 +1,33 @@
 //GLSL
 #version 330
 
+struct p3d_LightSourceParameters {
+  // Ambient is always vec4(0, 0, 0, 1)
+  vec4 ambient;
+  // light.get_color()
+  vec4 diffuse;
+  // light.get_specular_color()
+  vec4 specular;
+  // In case of Spotlight/PointLight, this is the light position in view space, with fourth component 1
+  // In case of DirectionalLight, this is actually (-dir) with the fourth component set to 0
+  vec4 position;
+  // This is the only one that's not actually available
+  vec4 halfVector;
+  // Etc blah blah same as OpenGL FFP equivalents
+  // You can just omit their declaration if you don't need them, they are bound by name
+  vec3 spotDirection;
+  float spotExponent;
+  float spotCutoff;
+  float spotCosCutoff;
+  float constantAttenuation;
+  float linearAttenuation;
+  float quadraticAttenuation;
+};
+
 uniform sampler2D p3d_Texture0; //color
 uniform sampler2D p3d_Texture1; //normal
 
-uniform gl_LightSourceParameters Light; //light
+uniform p3d_LightSourceParameters Light; //light
 
 in vec2 uv;
 in vec3 normal;
